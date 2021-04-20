8d074129f44ccbe880a7427312efb046d241b647
VP <vijeta.puthran@gmail.com>
2021-04-19 21:16:20 -0500

    Files updated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,6 +8,23 @@ function jsonData(sample) {
       Object.entries(result).forEach(([key, value]) => {
         panel.append("h6").text(`${key}: ${value}`);
       });    
+      console.log(result.wfreq);
+
+      var data = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: result.wfreq,
+          title: { text: "<b>Belly Button Washing Frequency<b><br>Scrubs per Week" },
+          type: "indicator",
+          mode: "gauge+number",
+          delta: { reference: 400 },
+          gauge: { axis: { range: [0, 9] },bar:{color:'red'} }
+        }
+      ];
+      
+      var layout = { width: 600, height: 400 };
+      Plotly.newPlot('gauge', data, layout);
+
     });
   }
 
@@ -59,3 +76,31 @@ Plotly.plot("bubble", bdata, blayout);
     Plotly.newPlot("bar", data, layout);
   });
 }
+//Initialize the page with a default plot
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+    
+    // Use the list of sample names to populate the select options
+    d3.json("samples.json").then((data) => {
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+    
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      plots(firstSample);
+      jsonData(firstSample);
+    });
+    }
+//This function is called when a dropdown menu item is selected        
+function optionChanged(newData) {
+    plots(newData);
+    jsonData(newData);
+    }       
+// Initialize the page with a default plot
+init();
